9ab4bb9a65db9cf06411fc508aa14f82212fbc0a
sgoen.1986 <sgoen.1986@695d4ece-01e9-11df-b136-7f7962f7bc17>
2010-01-15 18:01:16 +0000

    displaying the template in parent class through backtracing

--- app/controllers/HelloworldController.php
+++ app/controllers/HelloworldController.php
@@ -2,6 +2,8 @@
 class HelloworldController extends Shumvc_BaseWebController implements Shumvc_WebControllerInterface {
     public function index(){
         $message = 'Hello World';
-        include(dirname(__FILE__).'/../templates/helloworld.index.php');
+        
+        // calling the parents showTemplate function to display the tenplate in the browser
+        $this->showTemplate();
     }
 }
