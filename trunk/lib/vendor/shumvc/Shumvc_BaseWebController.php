3b148e538ab84669b29f68be4fb3c343dc3a103f
sgoen.1986 <sgoen.1986@695d4ece-01e9-11df-b136-7f7962f7bc17>
2010-01-15 18:01:16 +0000

    displaying the template in parent class through backtracing

--- trunk/lib/vendor/shumvc/Shumvc_BaseWebController.php
+++ trunk/lib/vendor/shumvc/Shumvc_BaseWebController.php
@@ -1,5 +1,18 @@
 <?php
 class Shumvc_BaseWebController {
-    private function showTemplate(){
+    public function showTemplate(){
+        $backtrace = debug_backtrace(__FUNCTION__);
+        
+        // get the function in lowercase
+        $function = strtolower($backtrace[1]['function']);
+        
+        // get the class in lowercase and excluse the "Controller" part
+        $tmp_class = $backtrace[1]['class'];
+        $class = strtolower(str_replace('Controller', '', $tmp_class));
+        
+        // create a string with the full path
+        $template_path = dirname(__FILE__).'/../../../app/templates/'.$class.'.'.$function.'.php';
+        
+        include($template_path);
     }
 }
