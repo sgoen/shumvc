d5a07eba787acb521dbac82c9f89e5acbd7113f7
sgoen.1986 <sgoen.1986@695d4ece-01e9-11df-b136-7f7962f7bc17>
2010-01-16 15:30:52 +0000

    Added config/config.php and moved bootstrap to config/ config.php defines variables for file access. Single point of configuration :)

--- trunk/web/index.php
+++ trunk/web/index.php
@@ -1,10 +1,15 @@
 <?php
+require_once(dirname(__FILE__).'/../lib/vendor/doctrine/Doctrine.php');
+require_once(dirname(__FILE__).'/../lib/vendor/shumvc/Shumvc.php');
+require_once(dirname(__FILE__).'/../lib/vendor/serpent/serpent.class.php');
+
+spl_autoload_register(array('Doctrine', 'autoload'));
+spl_autoload_register(array('Shumvc', 'autoload'));
+
 session_start();
 
-include('bootstrap.php');
-
-// Define the url in which the project exists for proper url handling
-define('DEPLOYMENT_URL', '/');
+include(dirname(__FILE__).'/../config/config.php');
+include(dirname(__FILE__).'/../config/bootstrap.php');
 
 $parsed_uri = parse_url($_SERVER['REQUEST_URI']);
 $uri = preg_replace(sprintf(':^%s:', DEPLOYMENT_URL), '', $parsed_uri['path']);
