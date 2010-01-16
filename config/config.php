51a07728d1fb5a993d1961a5e1127c208a0fb2e3
sgoen.1986 <sgoen.1986@695d4ece-01e9-11df-b136-7f7962f7bc17>
2010-01-16 16:29:05 +0000

    reshaped config file

--- config/config.php
+++ config/config.php
@@ -1,12 +1,25 @@
 <?php
-// Define the url in which the project exists for proper url handling
-define('DEPLOYMENT_URL', '/');
-// Define the directories
-define('DIR_APP', dirname(__FILE__).'/../app');
-define('DIR_TEMPLATES', DIR_APP.'/templates');
-define('DIR_TEMPLATES_C', DIR_APP.'/templates_c');
-// Define Serpent options
-define('SERPENT_FORCE_COMPILE', true);
-define('SERPENT_DEFAULT_RESOURCE', 'file');
-define('SERPENT_DEFAULT_COMPILER', 'serpent');
-define('SERPENT_CHARSET', 'utf-8');
+
+// First off, define the base dir off the app for easy set up of the other options
+define('DIR_BASEDIR', dirname(__FILE__).'/..');
+
+$array = array(
+    // Define the url in which the project exists for proper url handling
+    'DEPLOYMENT_URL' => '/',
+    
+    // Define the directories
+    'DIR_APP' => DIR_BASEDIR.'/app',
+    'DIR_TEMPLATES' => DIR_BASEDIR.'/app/templates',
+    'DIR_TEMPLATES_C' => DIR_BASEDIR.'/app/templates_c',
+    
+    // Define Serpent options
+    'SERPENT_FORCE_COMPILE' => true,
+    'SERPENT_DEFAULT_RESOURCE' => 'file',
+    'SERPENT_DEFAULT_COMPILER' => 'serpent',
+    'SERPENT_CHARSET' => 'utf-8',
+    
+    // Define the default page properties such as title etc.
+    'SHUMVC_APP_TITLE' => 'Shumvc - Hello World',
+);
+
+Shumvc::define_array($array, $keys);
