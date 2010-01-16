c66b40caabedb63b4d36378c5698b988ce63eacc
sgoen.1986 <sgoen.1986@695d4ece-01e9-11df-b136-7f7962f7bc17>
2010-01-16 17:12:24 +0000

    added 404 page, added rewriteconditions for access to media, style and js folders

--- app/templates/helloworld.index.tpl
+++ app/templates/helloworld.index.tpl
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>~~$shumvc_app.title~</title>
+        <link rel="stylesheet" type="text/css" href="style/default.css">
     </head>
     <body>
         <h1>~~$message~</h1>
---
+++ lib/vendor/shumvc/data/pages/404.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>404 Z0mb13s</title>
+<style>
+body{
+	background-color:#424242;
+	font-family:Arial, Helvetica, sans-serif;
+	color:#e5e5e5;
+	background-image:url(media/img/bg.png);
+	background-repeat:repeat-x;
+}
+h1{
+	font-size:55px;
+	color:#CCC;
+	margin-top:35px;
+}
+#wrapper{
+	margin-left:auto;
+	margin-right:auto;
+	width:1024px;
+}
+#top{
+	width:1024px;
+	float:left;
+	height:auto;
+	text-shadow:#999 -1px -1px;
+}
+#left{
+	float:left;
+	width:300px;
+	height:auto;
+}
+#right{
+	float:left;
+	width:724px;
+	height:590px;
+	background-image:url(media/img/zombie.png);
+	background-position:right;
+	background-repeat:no-repeat;
+}
+</style>
+</head>
+
+<body>
+<div id="wrapper">
+<div id="top">
+<h1>Error 404: A zombie ate your page!!! Oh noes!!111one!!! </h1>
+</div>
+<div id="left">
+<p>This page isn't working or does not exist! Please check the adress bar for typos.</p>
+</div>
+<div id="right">
+</div>
+</div>
+</body>
+</html>
