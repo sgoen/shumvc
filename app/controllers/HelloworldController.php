357d73fc5561cfceb19ce5e2d28af38037b978e4
sgoen.1986 <sgoen.1986@695d4ece-01e9-11df-b136-7f7962f7bc17>
2010-01-19 21:45:34 +0000

    removed test data

--- app/controllers/HelloworldController.php
+++ app/controllers/HelloworldController.php
@@ -2,7 +2,9 @@
 class HelloworldController extends Shumvc_BaseWebController implements Shumvc_WebControllerInterface {
     public function webIndex(){
         // Set vars for template viewing
-        $template_vars = array('message' => 'Hello World');
+        $template_vars = array(
+            'message' => 'Hello World',
+        );
         // calling the parents showTemplate function to display the tenplate in the browser
         $this->showTemplate($template_vars);
     }
