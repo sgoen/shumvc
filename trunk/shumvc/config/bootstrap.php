f2a023b8ef8af7f6c6dfd0209d1d1c419b7b5ac2
sgoen.1986 <sgoen.1986@695d4ece-01e9-11df-b136-7f7962f7bc17>
2010-03-07 19:56:41 +0000

    added exception handling for database connection (now in try catch)

--- trunk/shumvc/config/bootstrap.php
+++ trunk/shumvc/config/bootstrap.php
@@ -6,13 +6,17 @@ require_once(dirname(__FILE__).'/../lib/vendor/serpent/serpent.class.php');
 spl_autoload_register(array('Doctrine', 'autoload'));
 spl_autoload_register(array('Shumvc', 'autoload'));
 
-// Doctrine ...
-Doctrine::loadModels(DIR_MODELS);
-$manager = Doctrine_Manager::getInstance();
+try {
+	// Doctrine ...
+	Doctrine::loadModels(DIR_MODELS);
+	$manager = Doctrine_Manager::getInstance();
 
-// At this point no actual connection to the database is created
-$conn = Doctrine_Manager::connection(SHUMVC_DB_TYPE.'://'.SHUMVC_DB_USER.':'.SHUMVC_DB_PASS.'@'.SHUMVC_DB_HOST.'/'.SHUMVC_DB_NAME);
+	// At this point no actual connection to the database is created
+	$conn = Doctrine_Manager::connection(SHUMVC_DB_TYPE.'://'.SHUMVC_DB_USER.':'.SHUMVC_DB_PASS.'@'.SHUMVC_DB_HOST.'/'.SHUMVC_DB_NAME);
 
-// The first time the connection is needed, it is instantiated
-// This query triggers the connection to be created
-$conn->execute('SHOW TABLES');
+	// The first time the connection is needed, it is instantiated
+	// This query triggers the connection to be created
+	$conn->execute('SHOW TABLES');
+} catch (Exception $e){
+	echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
